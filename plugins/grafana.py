35a971cb4f7cc68c45b549f3b588148601bfd681
brxxn <brxxn@users.noreply.github.com>
2019-05-04 13:31:49 -0400

    [grafana] grafana bug fixes and track_active_members

--- plugins/grafana.py
+++ plugins/grafana.py
@@ -1,13 +1,13 @@
 from prometheus_client import start_http_server, CollectorRegistry
 from disco.bot import Plugin, Config
 import structures.grafana as grafana_structs
-import disco.types.base
 
 
 class GrafanaPluginConfig(Config):
 
     prometheus_server_port = 9091
     master_guild_id = 0
+    channel_blacklist = []
 
 
 @Plugin.with_config(GrafanaPluginConfig)
@@ -18,7 +18,10 @@ class GrafanaPlugin(Plugin):
         self.registry = CollectorRegistry()
         self.member_count_tracker = grafana_structs.MemberCountTracker(self.registry)
         self.message_count_tracker = grafana_structs.MessageCountTracker(self.registry)
+        self.active_member_tracker = grafana_structs.ActiveMemberTracker(self.registry)
         self.temporary_message_count = 0
+        self.active_members = []
+        self.cached_member_count = 0
         start_http_server(self.config.prometheus_server_port, registry=self.registry)
 
     @Plugin.schedule(30, True, True)
@@ -27,20 +30,29 @@ class GrafanaPlugin(Plugin):
         self.temporary_message_count = 0
 
     @Plugin.listen("MessageCreate")
-    def on_message(self, msg):
-        if msg.guild is None:
+    def on_message(self, message):
+        if message.guild is None:
             return
-        if msg.guild.id != self.config.master_guild_id:
+        if int(message.guild.id) != int(self.config.master_guild_id):
             return
+        if int(message.channel.id) in self.config.channel_blacklist:
+            return
+        if int(message.author.id) not in self.active_members:
+            self.active_members.append(message.author.id)
         self.temporary_message_count += 1
 
     @Plugin.schedule(30, True, True)
     def log_member_count(self):
         if self.config.master_guild_id not in self.client.state.guilds:
             return
-        member_count = self.client.state.guilds[self.config.master_guild_id].member_count
-        if member_count == 0:
-            print("skipping logging member count - guild has no members")
+        if self.client.state.guilds[self.config.master_guild_id].member_count == 0:
             return
+        member_count = len(self.client.state.guilds[self.config.master_guild_id].members)
         self.member_count_tracker.track_member_count(member_count)
+
+    @Plugin.schedule(3600, True, True)
+    def track_active_members(self):
+        active_members = len(self.active_members)
+        self.active_member_tracker.track_active_members(active_members)
+        self.active_members.clear()
 
--- structures/grafana.py
+++ structures/grafana.py
@@ -4,7 +4,7 @@ import prometheus_client as prom
 class MemberCountTracker:
 
     def __init__(self, registry):
-        self.member_count_tracker = prom.Gauge("members", "member count", registry=registry)
+        self.member_count_tracker = prom.Gauge("members", "member count (updated 30s)", registry=registry)
 
     def track_member_count(self, member_count):
         self.member_count_tracker.set(member_count)
@@ -13,7 +13,24 @@ class MemberCountTracker:
 class MessageCountTracker:
 
     def __init__(self, registry):
-        self.message_count_tracker = prom.Gauge("message_count", "number of messages per minute", registry=registry)
+        self.message_count_tracker = prom.Gauge("message_count", "number of messages per 30s", registry=registry)
 
     def track_message_count(self, message_count):
         self.message_count_tracker.set(message_count)
+
+
+class ActiveMemberTracker:
+    """
+    Tracks the amount of active members in a server.
+
+    *Active members* are defined as people who have sent a message in the last hour.
+    """
+
+    def __init__(self, registry):
+        self.active_member_tracker = prom.Gauge("active_members",
+                                                "amount of active members (sent a message within 1 hour)",
+                                                registry=registry)
+
+    def track_active_members(self, active_member_count):
+        self.active_member_tracker.set(active_member_count)
+
